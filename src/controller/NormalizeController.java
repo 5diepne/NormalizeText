f2040a039ad93977b91a611d5b4df83d62f5b26c
DiepTCNN <DiepTCNN@10.12.14.161>
2023-09-26 14:30:54 +0700

    NormalizeText

---
+++ src/controller/NormalizeController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import common.Algorithm;
+import common.Library;
+import view.Menu;
+
+/**
+ *
+ * @author DiepTCNN
+ */
+public class NormalizeController extends Menu<String> {
+
+    static String[] options = {"Normalize file text.", "Exit."};
+    Library l;
+    Algorithm ar;
+
+    public NormalizeController() {
+        super("PROGRAMING", options);
+        l = new Library();
+        ar = new Algorithm();
+    }
+
+    @Override
+    public void execute(int n) {
+        switch (n) {
+            case 1 -> ar.normalizeText("input.txt", "output.txt");
+            case 2 -> System.exit(0);
+        }
+    }
+}
+
